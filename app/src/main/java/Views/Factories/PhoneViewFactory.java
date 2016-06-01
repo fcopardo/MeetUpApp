f35cccad56ab83f63852d597b9141b52b4236159
fcopardo <francisco.pardo.baeza@gmail.com>
2016-06-01 16:32:42 -0400

    Adding comments

--- app/src/main/java/Views/Factories/PhoneViewFactory.java
+++ app/src/main/java/Views/Factories/PhoneViewFactory.java
@@ -16,7 +16,9 @@ public class PhoneViewFactory {
     public static BaseLandingView getLandingView(Context context){
 
         /*TODO: AQUI DEBERIAN FABRICARSE MUUUCHAS MAS VIEWS... SEGUN TAMAÑO, RAM DISPONIBLE,
-         CALIDAD DE LA RED, PAÍS, TIPO DE DISPOSITIVO...*/
+         CALIDAD DE LA RED, PAÍS, TIPO DE DISPOSITIVO... COMO EJEMPLO, REVISEMOS SI EL DISPOSITIVO
+         TIENE AL MENOS 1GB de RAM, y al menos 700 megas libres...
+         */
 
         BaseLandingView view;
         if(DeviceFunctions.isMemoryLow(context, 70000, 100000)) {
